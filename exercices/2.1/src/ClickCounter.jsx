1cdd184c99b8012443c0ed7948cacda91a69e55c
hajar <hajar.haouat@student.vinci.be>
2024-09-24 15:11:59 +0200

    new:ex2.1-stored

--- exercices/2.1/src/ClickCounter.jsx
+++ exercices/2.1/src/ClickCounter.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function ClickCounter({ title, message }) {
-  const [count, setCount] = useState(0);
-  const [hoverMessage, setHoverMessage] = useState('');
+  const initialCount = JSON.parse(localStorage.getItem("count")) || 0;
+  const [count, setCount] = useState(initialCount);
+  const [hoverMessage, setHoverMessage] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
     <>
       <h1>{title}</h1>
       <button
-        onClick={() => setCount((count) => count + 1)}
-        onMouseEnter={() => setHoverMessage('Please click on me now!')}
-        onMouseLeave={() => setHoverMessage('')}
+        onClick={handleClick}
+        onMouseEnter={() => setHoverMessage(true)}
+        onMouseLeave={() => setHoverMessage(false)}
       >
-        count is {count}
+        {hoverMessage ? "Click on me" : `count is ${count}`}
         {count >= 10 && <p>{message}</p>}
       </button>
-      {hoverMessage && <p>{hoverMessage}</p>}
     </>
   );
 }
