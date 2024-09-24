4a61905f965b8f172e920f266266be33d1ccb0c0
hajar <hajar.haouat@student.vinci.be>
2024-09-24 14:23:24 +0200

    new:ex2.1-destructuring

--- exercices/2.1/src/ClickCounter.jsx
+++ exercices/2.1/src/ClickCounter.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 
-function clickCounter() {
+function clickCounter({title, message}) {
     const [count, setCount] = useState(0)
 
     return(
         <>
+        <h1>{title}</h1>
         <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
+            {count >= 10 && <p>{message}</p>}
         </button>
         </>
     )
