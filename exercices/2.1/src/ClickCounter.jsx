3956a6b0f22d8eb462f8a93b9fc77c4495b3427f
hajar <hajar.haouat@student.vinci.be>
2024-09-24 15:00:22 +0200

    new:ex2.1-events

--- exercices/2.1/src/ClickCounter.jsx
+++ exercices/2.1/src/ClickCounter.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
+function ClickCounter({ title, message }) {
+  const [count, setCount] = useState(0);
+  const [hoverMessage, setHoverMessage] = useState('');
 
-function clickCounter({title, message}) {
-    const [count, setCount] = useState(0)
-
-    return(
-        <>
-        <h1>{title}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-            {count >= 10 && <p>{message}</p>}
-        </button>
-        </>
-    )
+  return (
+    <>
+      <h1>{title}</h1>
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        onMouseEnter={() => setHoverMessage('Please click on me now!')}
+        onMouseLeave={() => setHoverMessage('')}
+      >
+        count is {count}
+        {count >= 10 && <p>{message}</p>}
+      </button>
+      {hoverMessage && <p>{hoverMessage}</p>}
+    </>
+  );
 }
 
-export default clickCounter;
+export default ClickCounter;
