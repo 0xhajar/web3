d7a26d87543521773d6397cb4543d860110c39be
hajar <hajar.haouat@student.vinci.be>
2024-10-08 13:55:02 +0200

    first form

---
+++ exercices/4.1/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import Filter from './Components/Filter';
+import PersonForm from './Components/PersonForm';
+import Persons from './Components/Person';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleSearchChange = (event) => setSearchTerm(event.target.value);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      const personObject = { name: newName, number: newNumber };
+      setPersons(persons.concat(personObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  );
+};
+
+export default App;
---
+++ exercices/4.1/src/Components/Person.jsx
@@ -0,0 +1,12 @@
+const Persons = ({ personsToShow }) => (
+    <div>
+      {personsToShow.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+  
+  export default Persons;
+  
